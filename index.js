040a51ac6a80e5bee3a1a74b096e7b9b510eab32
tscotce <tscotce@gmail.com>
2017-11-21 04:27:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 drivers = ["Milo", "Otis", "Garfield"];
 function drivers(){
-  const drivers = [Milo, Otis, Garfield];
+  const drivers = (Milo, Otis, Garfield);
 }
 
 function destructivelyAppendDriver () {
